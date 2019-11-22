b1bcdc827d47e34aa34476c80b0b7a244f8d758b
Adam Hanbury-Brown <ahanburybrown@gmail.com>
2019-11-22 18:50:23 -0500

    made figures for 3 fires

---
+++ adams_theme.R
@@ -0,0 +1,9 @@
+
+adams_theme <- theme(plot.title = element_text(hjust = 0.5, size = 15),
+                     strip.text.x = element_text(size = 18),
+                     legend.title = element_blank (),
+                     axis.title.x = element_text (size = 13), # change the axis title
+                     axis.title.y = element_text (size = 13),
+                     axis.text.x = element_text (size = 14, colour = "black"),
+                     axis.text.y = element_text (size = 14, colour = "black"),
+                     legend.text = element_text (size = 15))
---
+++ create_recovery_trajectories.R
@@ -0,0 +1,50 @@
+library(ggplot2)
+library(tidyverse)
+
+source('adams_theme.R')
+
+bear <- read_csv(file = 'data/nbr_bear.csv', col_names = c("year","NBR"))
+#names(bear)
+
+bear_plot <- bear %>%
+  ggplot(aes(x=year,y=NBR))+
+  labs(title = "bear fire")+
+  geom_line()+
+  adams_theme
+
+png("output/bear_fire.png")
+bear_plot
+dev.off()
+
+
+panther <- read_csv(file = 'data/nbr_panther.csv', col_names = c("year","NBR"))
+
+panther_plot <- panther %>%
+  ggplot(aes(x=year,y=NBR))+
+  geom_line()+
+  labs(title = "panther fire") +
+  adams_theme
+
+png("output/panther_fire.png")
+panther_plot
+dev.off()
+
+
+
+yellow <- read_csv(file = 'data/nbr_yellow.csv', col_names = c("year","NBR"))
+  #names(panther)
+  
+yellow_fire_plot <- yellow %>%
+    ggplot(aes(x=year,y=NBR))+
+    geom_line()+
+    labs(title = "yellow fire") +
+   adams_theme
+  
+png("output/yellow_fire.png")
+yellow_fire_plot
+dev.off()
+
+
+
+
+
