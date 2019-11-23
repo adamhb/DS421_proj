fbfa6f45208a97daa75bb36f2de8c3c39f8f5e00
Adam Hanbury-Brown <ahanburybrown@gmail.com>
2019-11-23 15:29:13 -0500

    after added aspect data to the bear fire

--- scripts/create_recovery_trajectories.R
+++ scripts/create_recovery_trajectories.R
@@ -16,6 +16,32 @@ png("output/bear_fire.png")
 bear_plot
 dev.off()
 
+aspects <- c("north","south","east","west")
+
+bearNorth <- read_csv(file = 'data/nbr_bear_north.csv', col_names = c("year","NBR"))
+bearSouth <- read_csv(file = 'data/nbr_bear_south.csv', col_names = c("year","NBR"))
+bearEast <- read_csv(file = 'data/nbr_bear_east.csv', col_names = c("year","NBR"))
+bearWest <- read_csv(file = 'data/nbr_bear_west.csv', col_names = c("year","NBR"))
+
+aspectCol <- c()
+for(a in aspects){
+  temp <- rep(a,nrow(bearNorth))
+  aspectCol <- append(aspectCol,temp)
+}
+
+bearAspects <- rbind(bearNorth, bearSouth, bearEast, bearWest) %>% cbind(aspectCol)
+
+
+bearAspects %>%
+  ggplot(aes(year,NBR,col = aspectCol)) +
+  geom_line(size = 4) +
+  adams_theme
+
+
+
+
+
+
 
 panther <- read_csv(file = 'data/nbr_panther.csv', col_names = c("year","NBR"))
 
