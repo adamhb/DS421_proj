e8a2a15d94d83770d19235938c88d5921da829bc
Adam Hanbury-Brown <ahanburybrown@gmail.com>
2020-03-16 11:19:51 -0700

    created correlation matrix for 1987 fires

--- scripts/adams_theme.R
+++ scripts/adams_theme.R
@@ -1,7 +1,7 @@
 
 adams_theme <- theme(plot.title = element_text(hjust = 0.5, size = 15),
                      strip.text.x = element_text(size = 18),
-                     legend.title = element_blank (),
+                     #legend.title = element_blank (),
                      axis.title.x = element_text (size = 13), # change the axis title
                      axis.title.y = element_text (size = 13),
                      axis.text.x = element_text (size = 14, colour = "black"),
